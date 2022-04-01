25e22ee75e2638957f234ed7e12057a82b48a419
Barbie-6 <83456139+Barbie-6@users.noreply.github.com>
2022-04-01 18:24:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ objects = [];
 
 
 function setup() {
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(380, 380);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
@@ -13,7 +13,7 @@ function setup() {
     document.getElementById("status").innerHTML = "Detecting Objects";
 }
 function draw() {
-    image(video, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
     if(status != ""){
         object_detector.detect(img, gotResults);
         r = random(255);
@@ -21,6 +21,7 @@ function draw() {
         b = random(255);
         document.getElementById("status").innerHTML = "Objects Detected";
         for(i=0; i<objects.length; i++){
+             document.getElementById("number_of_objects").innerHTML = "Number of objects detected are " + objects.length ;
             label = objects[i].label;
             percent = floor(objects[i].confidence * 100);
             stroke(r,g,b);
